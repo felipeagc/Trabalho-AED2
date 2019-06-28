3df7a6a369293e10b5655fb7c3b4618fd45e9058
Felipe <felipeagc2@gmail.com>
2019-06-27 23:12:10 -0300

    Implementação da zona de colisão

--- main.cpp
+++ main.cpp
@@ -1,9 +1,11 @@
 #include "hash.hpp"
+#include <algorithm>
 #include <assert.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <vector>
 
 // Método de tratamento de colisões
 enum class Method : uint8_t {
@@ -37,6 +39,7 @@ struct Table {
   HashFn hash_fn;
   uint32_t entry_cap;
   Method method;
+  size_t zone_offset;
 
   Table(const char *path, HashFn hash_fn, size_t entry_cap, Method method) {
     this->method = method;
@@ -46,12 +49,21 @@ struct Table {
     this->hash_fn   = hash_fn;
     this->entry_cap = entry_cap;
 
-    fseek(this->file, 0, SEEK_SET);
-    if (ftell(this->file) == 0) {
+    switch (this->method) {
+    case Method::NONE:
+    case Method::COLLISION_ZONE: {
+      this->zone_offset = sizeof(FileHeader) + (sizeof(Slot) * this->entry_cap);
+      break;
+    }
+    default: break;
+    }
+
+    if (file_size() == 0) {
       FileHeader header = {};
       header.entry_cap  = this->entry_cap;
 
-      write_at(0, sizeof(header), &header);
+      bool wrote = write_at(0, sizeof(header), &header);
+      assert(wrote);
     }
   }
 
@@ -69,7 +81,12 @@ struct Table {
     return written == 1;
   }
 
-  bool insert(const Entry &entry) {
+  inline size_t file_size() {
+    fseek(this->file, 0, SEEK_END);
+    return ftell(this->file);
+  }
+
+  bool insert(const Entry &entry, bool *collided) {
     uint32_t hash = this->hash_fn(entry.id) % this->entry_cap;
 
     switch (this->method) {
@@ -78,13 +95,46 @@ struct Table {
 
       size_t pos = sizeof(FileHeader) + (sizeof(slot) * hash);
       if (read_at(pos, sizeof(slot), &slot)) {
-        if (slot.filled) return false;
+        if (slot.filled) {
+          if (collided) *collided = true;
+          return false;
+        }
       }
 
       slot.entry  = entry;
       slot.filled = true;
 
-      write_at(pos, sizeof(slot), &slot);
+      bool wrote = write_at(pos, sizeof(slot), &slot);
+      assert(wrote);
+
+      if (collided) *collided = false;
+      return true;
+    }
+    case Method::COLLISION_ZONE: {
+      Slot slot = {};
+
+      size_t pos = sizeof(FileHeader) + (sizeof(slot) * hash);
+      if (read_at(pos, sizeof(slot), &slot) && slot.filled) {
+        pos = std::max(file_size(), this->zone_offset);
+
+        // Inserir na zona de colisão
+        slot.entry  = entry;
+        slot.filled = true;
+        bool wrote  = write_at(pos, sizeof(slot), &slot);
+        assert(wrote);
+
+        if (collided) *collided = true;
+
+        return true;
+      }
+
+      // Não houve colisão, escrever slot normalmente
+      if (collided) *collided = false;
+
+      slot.entry  = entry;
+      slot.filled = true;
+      bool wrote  = write_at(pos, sizeof(slot), &slot);
+      assert(wrote);
 
       return true;
     }
@@ -92,7 +142,7 @@ struct Table {
     }
   }
 
-  bool search(const uint32_t id, Entry *entry) {
+  bool search(const uint32_t id, Entry *entry, size_t *file_pos) {
     uint32_t hash = this->hash_fn(id) % this->entry_cap;
 
     switch (this->method) {
@@ -103,9 +153,38 @@ struct Table {
       if (read_at(pos, sizeof(slot), &slot)) {
         if (!slot.filled) return false;
         if (slot.entry.id != id) return false;
+
         if (entry != nullptr) *entry = slot.entry;
+        if (file_pos != nullptr) *file_pos = pos;
 
         return true;
+      }
+
+      return false;
+    }
+    case Method::COLLISION_ZONE: {
+      Slot slot = {};
+
+      size_t pos = sizeof(FileHeader) + (sizeof(slot) * hash);
+      if (read_at(pos, sizeof(slot), &slot)) {
+        if (slot.filled && slot.entry.id == id) {
+          if (entry != nullptr) *entry = slot.entry;
+          if (file_pos != nullptr) *file_pos = pos;
+          return true;
+        }
+      }
+
+      // Busca linear no final do arquivo
+      for (pos = this->zone_offset; pos + sizeof(Slot) <= file_size();
+           pos += sizeof(Slot)) {
+        if (read_at(pos, sizeof(slot), &slot)) {
+          if (slot.filled && slot.entry.id == id) {
+            // Achou
+            if (entry != nullptr) *entry = slot.entry;
+            if (file_pos != nullptr) *file_pos = pos;
+            return true;
+          }
+        }
       }
 
       return false;
@@ -114,29 +193,29 @@ struct Table {
     }
   }
 
-  void remove(const uint32_t id) {
-    if (!search(id, NULL)) {
-      return;
+  bool remove(const uint32_t id) {
+    size_t pos;
+    if (!search(id, NULL, &pos)) {
+      return false;
     }
 
-    uint32_t hash = this->hash_fn(id) % this->entry_cap;
-
     switch (this->method) {
-    case Method::NONE: {
+    case Method::NONE:
+    case Method::COLLISION_ZONE: {
       Slot slot   = {};
       slot.filled = false;
 
-      size_t pos = sizeof(FileHeader) + (sizeof(slot) * hash);
-      write_at(pos, sizeof(slot), &slot);
+      bool wrote = write_at(pos, sizeof(slot), &slot);
+      assert(wrote);
 
-      break;
+      return true;
     }
-    default: return;
+    default: return false;
     }
   }
 };
 
-void test_collisions(Table &table) {
+void test(Table &table) {
   FILE *file = fopen("../data.csv", "r");
   assert(file);
 
@@ -146,6 +225,8 @@ void test_collisions(Table &table) {
   fgets(buf, sizeof(buf), file);
 
   uint32_t collisions = 0;
+
+  std::vector<Entry> entries;
 
   while (fgets(buf, sizeof(buf), file) != NULL) {
     Entry entry;
@@ -167,8 +248,13 @@ void test_collisions(Table &table) {
     pt = strtok(NULL, ",");
     strncpy(entry.phone, pt, 13); // celular
 
-    if (entry.line >= 70000 && entry.line <= 80000) {
-      if (!table.insert(entry)) {
+    if (entry.line >= 70000 && entry.line < 80000) {
+      bool collided;
+      if (table.insert(entry, &collided)) {
+        entries.push_back(entry);
+      }
+
+      if (collided) {
         collisions++;
       }
     }
@@ -176,24 +262,39 @@ void test_collisions(Table &table) {
 
   printf("=> Colisões: %u\n", collisions);
 
+  // Testa se a busca funciona (muito lento com zona de colisão)
+  // for (const Entry &entry : entries) {
+  //   bool found = table.search(entry.id, NULL, NULL);
+  //   assert(found);
+  // }
+
+  // Testa se a remoção funciona (muito lento com zona de colisão)
+  // for (const Entry &entry : entries) {
+  //   bool removed = table.remove(entry.id);
+  //   assert(removed);
+
+  //   bool found = table.search(entry.id, nullptr, &pos);
+  //   assert(!found);
+  // }
+
   fclose(file);
 }
 
 const char *method_name(Method method) {
   switch (method) {
   case Method::NONE: return "nenhum";
-  case Method::CHAIN_HEAD: return "encadeamento com cabeça";
-  case Method::CHAIN_NO_HEAD: return "encadeamento sem cabeça";
-  case Method::COLLISION_ZONE: return "zona de colisão";
+  case Method::CHAIN_HEAD: return "encadeamento-com-cabeca";
+  case Method::CHAIN_NO_HEAD: return "encadeamento-sem-cabeca";
+  case Method::COLLISION_ZONE: return "zona";
   }
   return "";
 }
 
 const char *hash_fn_name(HashFn fn) {
-  if (fn == hash_mult) return "multiplicação";
-  if (fn == hash_mult_quad) return "multiplicação (quadrado)";
-  if (fn == hash_divisao) return "divisão";
-  if (fn == hash_divisao_primo) return "divisão (primo)";
+  if (fn == hash_mult) return "mult";
+  if (fn == hash_mult_quad) return "mult_quad";
+  if (fn == hash_divisao) return "divisao";
+  if (fn == hash_divisao_primo) return "divisao_primo";
   if (fn == hash_dobra) return "dobra";
   return "";
 }
@@ -201,20 +302,27 @@ const char *hash_fn_name(HashFn fn) {
 int main() {
   uint32_t table_size = 10000;
 
-  Method methods[]  = {Method::NONE};
+  Method methods[]  = {Method::NONE, Method::COLLISION_ZONE};
   HashFn hash_fns[] = {
       hash_mult, hash_mult_quad, hash_divisao, hash_divisao_primo, hash_dobra};
 
   for (const Method &method : methods) {
     for (const HashFn &hash_fn : hash_fns) {
-      Table table("./table.bin", hash_fn, table_size, method);
+      char filepath[512] = "";
+      sprintf(
+          filepath,
+          "./tabela_%s_%s.bin",
+          method_name(method),
+          hash_fn_name(hash_fn));
+
+      Table table(filepath, hash_fn, table_size, method);
 
       printf(
           "Tratamento de colisões: %s\nFunção: %s\n",
           method_name(method),
           hash_fn_name(hash_fn));
 
-      test_collisions(table);
+      test(table);
 
       printf("\n");
     }
